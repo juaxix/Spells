4ce3404d6a30dc565c3aa6e34b8e38d4d6466093
Juan Belon <juaxix@gmail.com>
2021-12-22 12:21:52 +0100

    - Added Get all players EQS. - Created a EQS Test Pawn. - Fixed the enemy health bar look at cam.

--- Source/Spells/SpellsGameModeBase.cpp
+++ Source/Spells/SpellsGameModeBase.cpp
@@ -1,5 +1,24 @@
 // Spells - xixgames - juaxix - 2021/2022
 
-
 #include "SpellsGameModeBase.h"
 
+/// Unreal includes
+#include "GameFramework/GameStateBase.h" 
+#include "GameFramework/PlayerState.h"
+#include "Player/SCharacter.h"
+
+// @todo move to the game state (accessible by all)
+TArray<ASCharacter*> ASpellsGameModeBase::GetAllPlayers() const
+{
+	TArray<ASCharacter*> Characters;
+	Characters.Reserve(GameState->PlayerArray.Num());
+	for (const APlayerState* Player : GameState->PlayerArray)
+	{
+		if (ASCharacter* Character = Cast<ASCharacter>(Player->GetPawn()))
+		{
+			Characters.Emplace(Character);
+		}
+	}
+
+	return Characters;
+}
--- Source/Spells/SpellsGameModeBase.h
+++ Source/Spells/SpellsGameModeBase.h
@@ -6,12 +6,14 @@
 #include "GameFramework/GameModeBase.h"
 #include "SpellsGameModeBase.generated.h"
 
-/**
- * 
- */
+class ASCharacter;
+
 UCLASS()
 class SPELLS_API ASpellsGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+
+public:
+	UFUNCTION(BlueprintPure, Category = "Spells|Game Mode")
+	TArray<ASCharacter*> GetAllPlayers() const;
 };
