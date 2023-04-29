385ecbae62e83780823ab733dbec18cb4244a5b5
Juan Belon <juaxix@gmail.com>
2023-04-29 22:34:25 +0200

    Update to UE 5.1

--- Source/Spells.Target.cs
+++ Source/Spells.Target.cs
@@ -9,7 +9,7 @@ public class SpellsTarget : TargetRules
 	{
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
-
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.AddRange( new string[] { "Spells" } );
 	}
 }
--- Source/SpellsEditor.Target.cs
+++ Source/SpellsEditor.Target.cs
@@ -9,7 +9,7 @@ public class SpellsEditorTarget : TargetRules
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
-
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.AddRange( new string[] { "Spells" } );
 	}
 }
